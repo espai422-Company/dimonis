99915e75db634f9ba98ff88dbf37e862d19c6eba
Benny <peptoni0345@gmail.com>
2023-12-27 16:54:45 +0100

    Added all models to models.dart

--- lib/models/models.dart
+++ lib/models/models.dart
@@ -1,2 +1,4 @@
 export 'package:app_dimonis/models/dimoni.dart';
 export 'package:app_dimonis/models/firebase_model.dart';
+export 'package:app_dimonis/models/gimcama.dart';
+export 'package:app_dimonis/models/progres.dart';
