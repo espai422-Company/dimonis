061595d6e8883aedf90feb436be81e32b93010d5
Andreu Garcia <andreustreaming@gmail.com>
2023-12-27 16:57:41 +0100

    Llista de gimcames funcionant

--- lib/widgets/gimcama_list.dart
+++ lib/widgets/gimcama_list.dart
@@ -1,6 +1,9 @@
 import 'package:app_dimonis/models/gimcama.dart';
+import 'package:app_dimonis/providers/playing_gimcama.dart';
+import 'package:app_dimonis/providers/ui_provider.dart';
 import 'package:app_dimonis/widgets/gimcama_card.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class GimcamaList extends StatelessWidget {
   GimcamaList({super.key});
@@ -19,7 +22,15 @@ class GimcamaList extends StatelessWidget {
             return ListView.builder(
               itemCount: snapshot.data!.length,
               itemBuilder: (context, index) {
-                return GimcamaCard(gimcama: snapshot.data![index]);
+                return GestureDetector(
+                  onTap: () {
+                    Provider.of<PlayinGimcamaProvider>(context, listen: false)
+                        .currentGimcama = snapshot.data![index];
+                    Provider.of<UIProvider>(context, listen: false)
+                        .selectMenuOpt = 0;
+                  },
+                  child: GimcamaCard(gimcama: snapshot.data![index]),
+                );
               },
             );
           }
