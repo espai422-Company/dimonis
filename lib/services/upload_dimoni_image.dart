5ac4a0ad363e992cb43a5e2349d079d0dc8b169f
Elloberas04 <estevellobera@paucasesnovescifp.cat>
2023-12-29 01:47:06 +0100

    Millora upload dimoni

--- lib/services/upload_dimoni_image.dart
+++ lib/services/upload_dimoni_image.dart
@@ -5,10 +5,8 @@ import 'package:firebase_storage/firebase_storage.dart';
 
 final FirebaseStorage storage = FirebaseStorage.instance;
 
-Future<bool> uploadImage(File image) async {
-  final String nameFile = image.path.split('/').last;
-
-  Reference ref = storage.ref().child(nameFile);
+Future<bool> uploadImage(File image, String nomImage) async {
+  Reference ref = storage.ref().child(nomImage);
 
   final UploadTask uploadTask = ref.putFile(image);
 
@@ -24,6 +22,12 @@ Future<bool> uploadImage(File image) async {
 Future<bool> uploadDimoni(Dimoni dimoniTemp, File image) async {
   try {
     final String nameFile = image.path.split('/').last;
+
+    final uploadedImage = await uploadImage(image, nameFile);
+
+    if (!uploadedImage) {
+      return false;
+    }
 
     dimoniTemp.image =
         'https://firebasestorage.googleapis.com/v0/b/appdimonis.appspot.com/o/$nameFile?alt=media&token=8eff69df-c8ce-4af1-b0d9-7021985158a5';
