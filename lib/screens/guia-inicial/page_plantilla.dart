10ed495d0ab86325c8c6d227d4f88ae35f5cfdc7
Elloberas04 <estevellobera@paucasesnovescifp.cat>
2024-02-15 23:01:19 +0100

    Initial guide with 2 more plugins and more changes

---
+++ lib/screens/guia-inicial/page_plantilla.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class PagePlantilla extends StatelessWidget {
+  const PagePlantilla({
+    super.key,
+    required this.image,
+    required this.title,
+    required this.subTitle,
+    required this.counterText,
+    required this.bgColor,
+    this.size,
+  });
+
+  final String image;
+  final String title;
+  final String subTitle;
+  final String counterText;
+  final Color bgColor;
+  final double? size;
+
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    return Container(
+      padding: const EdgeInsets.all(20),
+      color: bgColor,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Image(
+            image: AssetImage(image),
+            height: size.height * 0.45,
+          ),
+          Column(
+            children: [
+              Text(
+                title,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.w900,
+                  color: Theme.of(context).primaryColor,
+                ),
+              ),
+              Text(
+                subTitle,
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+          Text(
+            counterText,
+            style: Theme.of(context).textTheme.titleLarge,
+          ),
+          const SizedBox(
+            height: 80.0,
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/login-register/sing_up_screen.dart
+++ lib/screens/login-register/sing_up_screen.dart
@@ -1,3 +1,4 @@
+import 'package:app_dimonis/preferences/preferences.dart';
 import 'package:app_dimonis/screens/login-register/components_login.dart';
 import 'package:app_dimonis/screens/screens.dart';
 import 'package:app_dimonis/services/auth.dart';
@@ -68,11 +69,11 @@ class _SignUpFormWidget extends State<SignUpFormWidget> {
 
     try {
       await Auth().registerWithEmailAndPassword(email, password, user);
-      Navigator.of(context).push(
-        MaterialPageRoute(
-          builder: (context) => const ComprovacioLogin(),
-        ),
-      );
+      if (Preferences.guiaInicial) {
+        Navigator.of(context).pushNamed('guia_inicial');
+      } else {
+        Navigator.of(context).pushNamed('/');
+      }
       _emailController.text = '';
       _passController.text = '';
       _userNameController.text = '';
