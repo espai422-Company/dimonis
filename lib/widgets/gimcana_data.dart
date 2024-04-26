960c07fc2a6f210f4dad935d644543d62e838eed
joan <joansanchez@paucasesnovescifp.cat>
2024-04-26 15:16:41 +0200

    leaderboard

--- lib/widgets/gimcana_data.dart
+++ lib/widgets/gimcana_data.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:app_dimonis/models/firebase/firebase_user.dart';
 import 'package:app_dimonis/models/state/progress.dart';
+import 'package:app_dimonis/preferences/preferences.dart';
 import 'package:app_dimonis/providers/firebase_provider.dart';
 import 'package:app_dimonis/providers/progress_provider.dart';
 import 'package:flutter/material.dart';
@@ -10,41 +11,83 @@ import 'package:provider/provider.dart';
 class GimcamaData extends StatelessWidget {
   const GimcamaData({super.key});
 
-
   @override
   Widget build(BuildContext context) {
-    var progressProvider = Provider.of<FireBaseProvider>(context, listen: true).progressProvider;
-    Map<FirebaseUser,Progress> progress = progressProvider.progressMap;
+    var progressProvider =
+        Provider.of<FireBaseProvider>(context, listen: true).progressProvider;
+    // Map<FirebaseUser,Progress> progress = progressProvider.progressMap;
 
     return Padding(
       padding: const EdgeInsets.all(20.0),
-      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-        SizedBox(
-          height: 300,
-          width: 300,
-          child: ListView.builder(
-            shrinkWrap: true,
-            itemCount: progressProvider.progressMap.length,
-            itemBuilder: (context, index) {
-              var user = progressProvider.progressMap.keys.elementAt(index);
-              var progress = progressProvider.progressMap[user]!;
-              dynamic count;
+      child:
+          Column(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+        // SizedBox(
+        //   height: 300,
+        //   child:
+        ListView.builder(
+          shrinkWrap: true,
+          itemCount: progressProvider.progressMap.length,
+          itemBuilder: (context, index) {
+            var user = progressProvider.progressMap.keys.elementAt(index);
+            var progress = progressProvider.progressMap[user]!;
+            Widget count;
 
-              count = getTime(progressProvider, user, progress);
+            count = getTime(progressProvider, user, progress);
 
-              return ListTile(
-                  title: Text("${index + 1} ${user.displayName}"),
-                  subtitle: count,
-                  trailing: Text(progress.discovers.length.toString()),
-                );
-            },
-          ),
+            return Container(
+              padding: const EdgeInsets.all(5),
+              margin: const EdgeInsets.only(bottom: 10),
+              decoration: BoxDecoration(
+                  color: index == 0
+                      ? const Color.fromARGB(137, 216, 187, 26)
+                      : (index == 1
+                          ? const Color.fromARGB(127, 205, 206, 205)
+                          : (index == 2
+                              ? const Color.fromARGB(123, 187, 121, 0)
+                              : Preferences.isDarkMode
+                                  ? const Color.fromARGB(137, 255, 124, 124)
+                                  : const Color.fromARGB(155, 255, 127, 127))),
+                  borderRadius: BorderRadius.circular(10),
+                  border: Border.all(
+                    width: index == 0
+                        ? 5
+                        : (index == 1 ? 4 : (index == 2 ? 3 : 3)),
+                    color: index == 0
+                        ? const Color.fromARGB(255, 216, 188, 26)
+                        : (index == 1
+                            ? const Color.fromARGB(255, 205, 206, 205)
+                            : (index == 2
+                                ? const Color.fromARGB(255, 187, 121, 0)
+                                : const Color.fromARGB(0, 255, 255, 255))),
+                  )),
+              child: ListTile(
+                leading: index == 0
+                    ? Image.asset(
+                        "assets/fotos_clasificacio/Oro.png",
+                        width: 20,
+                      )
+                    : (index == 1
+                        ? Image.asset(
+                            "assets/fotos_clasificacio/Plata.png",
+                            width: 20,
+                          )
+                        : (index == 2
+                            ? Image.asset(
+                                "assets/fotos_clasificacio/Bronze.png",
+                                width: 20,
+                              )
+                            : Container(width: 20))),
+                title: Text(user.displayName),
+                subtitle: count,
+                trailing: Text(progress.discovers.length.toString()),
+              ),
+            );
+          },
         ),
-        const Expanded(child: SizedBox()),
-        ElevatedButton(
+        // ),
+        FloatingActionButton(
             onPressed: () => progressProvider.unSubscribe(),
-            child: const Text("Tornar a les gimcanes")),
-        const Expanded(child: SizedBox()),
+            child: const Icon(Icons.exit_to_app)),
       ]),
     );
   }
@@ -53,22 +96,22 @@ class GimcamaData extends StatelessWidget {
     if (progressProvider.timeToComplete[user] == null) {
       DateTime primeraCaptura = DateTime.parse(progress.discovers[0].time);
       Duration difference = primeraCaptura.difference(DateTime.now());
-    
+
       int days = difference.inDays * -1;
       int hours = difference.inHours.remainder(24) * -1;
       int minutes = difference.inMinutes.remainder(60) * -1;
       int seconds = difference.inSeconds.remainder(60) * -1;
-    
-      return CountUp(days: days, hours: hours, minutes: minutes, seconds: seconds);
-      
+
+      return CountUp(
+          days: days, hours: hours, minutes: minutes, seconds: seconds);
     } else {
       Duration difference = progressProvider.timeToComplete[user]!;
-    
+
       int days = difference.inDays * -1;
       int hours = difference.inHours.remainder(24) * -1;
       int minutes = difference.inMinutes.remainder(60) * -1;
       int seconds = difference.inSeconds.remainder(60) * -1;
-    
+
       return Text("Completat en ${days}d ${hours}h ${minutes}m ${seconds}s");
     }
   }
